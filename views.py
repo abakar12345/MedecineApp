1a6467aa2967a2a24c3e163ab937949a9840adf2
abakar12345 <abakarmedi@gmail.com>
2023-10-30 17:22:47 +0100

    Premier commit

---
+++ views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+
+def index(request):
+    context={"message":"Bonsoir"}
+    template = loader.get_template('MedecineApp/index.html')
+    return HttpResponse(template.render(context,request))
+
